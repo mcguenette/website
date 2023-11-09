00685a9c0853165431bc2eb461374915552871ca
MC Guenette <mc.c.guenette@gmail.com>
2023-11-09 12:51:09 -0600

    Start authentication

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 'use strict';
 
-const TOKEN = '';
+const TOKEN = '35345345352234';
+
+
+function selectElement(selector) {
+    return document.querySelector(selector);
+}
+
+
+function printMenu () {
+    // fetch the menu
+    // forEach ...
+    // Add the content to the index.html
+}
